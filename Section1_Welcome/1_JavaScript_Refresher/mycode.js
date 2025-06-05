93d357edca9292fe2875a8f7612528aa7159cfca
jyoon66 <asian.tiger.juice@gmail.com>
2025-06-05 09:05:52 -0700

    Jun 6 2025 Files

---
+++ Section1_Welcome/1_JavaScript_Refresher/mycode.js
@@ -0,0 +1,22 @@
+// Prints the string "hi" to the terminal
+console.log("hi");
+
+// Prints the string "bye" to the terminal
+console.log("bye");
+
+
+// Examples of checking data types
+const message = "hello";
+console.log(typeof(message)); // Output: string
+
+const isLoggedIn = false;
+console.log(typeof(isLoggedIn)); // Output: boolean
+
+const userAge = 25;
+console.log(typeof(userAge)); // Output: number
+
+const isFalseString = "false"; // Note the quotes
+console.log(typeof(isFalseString)); // Output: string
+
+const isOneNumber = 1; // Note no quotes
+console.log(typeof(isOneNumber)); // Output: number
--- mycode.js
+++
@@ -1,4 +0,0 @@
-// let myVar = "hi"
-const myVar = "hi"
-// This next line prints out myVar
-console.log(myVar)
