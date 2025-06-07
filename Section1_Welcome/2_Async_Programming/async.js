6d4dcdb2f06e03b2369d30cbaa8c349cfba00700
jyoon66 <asian.tiger.juice@gmail.com>
2025-06-06 23:15:53 -0700

    Jun 6 2025 Files

---
+++ Section1_Welcome/2_Async_Programming/async.js
@@ -0,0 +1,15 @@
+// Synchronous Example
+function main() {
+  console.log("Step 1: Start");
+  let message = "Step 2: Processing...";
+  console.log(message);
+  console.log("Step 3: Finish");
+}
+
+main();
+// Output:
+// Step 1: Start
+// Step 2: Processing...
+// Step 3: Finish
+
+
